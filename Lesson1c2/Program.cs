94feb3b1aebbca3b2d14df659abac7f463b0b069
trexa81 <trex81@yandex.ru>
2021-12-09 07:12:14 +0300

    new branch

--- Lesson1c2/Program.cs
+++ Lesson1c2/Program.cs
@@ -6,8 +6,19 @@ namespace Lesson1c2  // Виктор Маликов
     {
         static void Main(string[] args)
         {
-
-        } 
-
-    }
+            int n = Console.ReadLine();
+            Console.WriteLine($"Output: {Reverse(n)}");
+        }
+        public static long Reverse(long n)
+        //В данном случае ввод — это просто аргумент функции. Просто входной параметр.
+        {
+            long newN = 0;
+            while (n > 0)
+            {
+                newN = newN * 10 + n % 10;
+                n /= 10;
+            }
+            return newN;
+        }
+    } 
 }
